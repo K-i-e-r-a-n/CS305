b58639e4915708769b4315c458c360d42b3db05a
K-i-e-r-a-n <renoh47@gmail.com>
2014-09-01 18:09:12 -0700

    Added more comments to hw1

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -12,6 +12,20 @@
 #define MIN_ARGUMENTS 2  //minimum number of expected arguments
 #define MAX_ARGUMENTS 5 //maximum number of expected arguments
 
+
+/*	Function main()
+*
+*	Desc: Runs the main program. 1 to 4 command line arguments are read in as an integer amount of kg, then 
+*	the value is converted to an equivalent amount of Toyota Priuses and printed out.
+*
+*	Parameters:
+*	-argc: number of command line arguments, including name of executable
+* 	-argv: array of strings that are the command line arguments
+*  
+*   Returns:
+*	EXIT_FAILURE if there are less than 2 command line arguments or more than 5, including executable name.
+*	EXIT_SUCCESS otherwise.
+*/
 int main(int argc, char** argv)
 {
 	if (argc < MIN_ARGUMENTS) // if there are less arguments than expected, print error and correct usage, then exit with failure status
@@ -20,7 +34,7 @@ int main(int argc, char** argv)
 		printf("usage: ./hw1 arg1 [arg2 ... arg4].\n");
 		return EXIT_FAILURE;
 	}
-	else if (argc > MAX_ARGUMENTS) //if there are too many arguments, print error and usage, then exit with failure status
+	else if (argc > MAX_ARGUMENTS) //if there are too many arguments, print error and correct usage, then exit with failure status
 	{
 		printf("Error: Too many arguments.\n");
 		printf("usage: ./hw1 arg1 [arg2 ... arg4].\n");
@@ -28,9 +42,9 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		printf("%d parameters.\n", argc);
-		int i = 0;
-		for (i = 1; i < argc; i++) // starting at the 2nd argument, loop through the last argument
+		printf("%d parameters.\n", argc); //print nuber of command line arguments entered.
+		int i = 0; //index for loop.
+		for (i = 1; i < argc; i++) // looping through from the 2nd to the number of arguments.
 		{
 			printf("%s kg = %7f TP.\n", argv[i], atoi(argv[i]) * KG_TO_PRIUS); // print kg and TP equivalent
 		}
